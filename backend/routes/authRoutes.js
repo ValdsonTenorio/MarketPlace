bf657a737a6db9d0d55ba96b0acae61d60e65b8a
Valdson <valdsonmartinstenorio721@gmail.com>
2025-08-14 12:59:46 -0400

    Criaçao da rota de produtos

--- backend/routes/authRoutes.js
+++ backend/routes/authRoutes.js
@@ -1,10 +1,14 @@
 import express from "express";
 import { register, login } from "../controllers/authController.js";
+import { verifyToken, verifyRole } from "../middleware/authMiddleware.js";
 
 const router = express.Router();
 
 router.post("/register", register);
 
 router.post("/login", login);
+
+router.post("/products", verifyToken, verifyRole("vendedor"), createProduct);
+
 
 export default router;
